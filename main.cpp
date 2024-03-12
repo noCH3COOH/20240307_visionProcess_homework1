305ea201a1c5c3ceaa114add7c274cee33d02ada
noCH3COOH <13178669726@163.com>
2024-03-12 11:49:28 +0800

    20240312_1149
    fix：完成部分框架

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     ifstream src;
-    src.open("test.yuz", ios::binary);
+    src.open("./video/test.yuv");
     if(!src.is_open())
     {
         cout << "[ERROR] 文件未打开" << endl;
@@ -18,3 +18,27 @@ int main()
 
     return 0;
 }
+
+
+void yuv2rgb888_1p(uint8_t* src_pixel)
+{
+    uint8_t src_yuv_pixel[3];
+    src_yuv_pixel[0] = src_pixel[0];
+    src_yuv_pixel[1] = src_pixel[1] - 128;
+    src_yuv_pixel[2] = src_pixel[2] - 128;
+
+    src_pixel[0] = src_yuv_pixel[0] + (-0.00093) * src_yuv_pixel[1] + 1.401687 * src_yuv_pixel[2];
+    src_pixel[1] = src_yuv_pixel[0] + (-0.3437) * src_yuv_pixel[1] + (-0.71417) * src_yuv_pixel[2];
+    src_pixel[2] = src_yuv_pixel[0] + 1.77216 * src_yuv_pixel[1] + 0.00099 * src_yuv_pixel[2];
+}
+
+void yuv2rgb888_all(enum PIXEL_FMT FMT)
+{
+
+}
+
+void play_VRAM(void)
+{
+
+}
+
